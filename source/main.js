d096faa2516e8dc5e90a3a035b5d1f48bb98e2b1
Matt Hayes <matt@mysterycommand.com>
2017-08-25 11:37:18 -0400

    refactor: break man and ex into functions with helpers

--- source/main.js
+++ source/main.js
@@ -1,25 +1,46 @@
+const { PI: π } = Math;
+
 const cvs = document.getElementById('c');
 const ctx = cvs.getContext('2d');
 
 const w = (cvs.width = 160);
 const h = (cvs.height = 90);
 
+const hw = w / 2;
+const hh = h / 2;
+
 ctx.imageSmoothingEnabled = false;
 ctx.fillStyle = 'red';
 ctx.fillRect(0, 0, w, h);
 
 ctx.fillStyle = 'darkred';
-ctx.fillRect(w / 2 - 0, h / 2 - 8, 2, 4);
-ctx.fillRect(w / 2 - 2, h / 2 - 4, 4, 8);
-ctx.fillRect(w / 2 - 4, h / 2 + 4, 2, 4);
-ctx.fillRect(w / 2 + 2, h / 2 + 4, 2, 4);
-ctx.fillRect(w / 2 - 0, h / 2 + 16, 2, 4);
-ctx.fillRect(w / 2 - 4, h / 2 + 16, 4, 2);
 
-// ctx.lineWidth = 1;
-// ctx.strokeStyle = 'white';
-// ctx.moveTo(0, 0);
-// ctx.lineTo(w, h);
-// ctx.moveTo(w, 0);
-// ctx.lineTo(0, h);
-// ctx.stroke();
+function rect(x, y, s, r = 0) {
+  ctx.translate(x, y);
+  ctx.rotate(r);
+  ctx.fillRect(s * -1, s * -2, s * 2, s * 4);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+function man(x, y) {
+  rect(x + 1, y - 6, 1);
+  rect(x, y, 2);
+  rect(x - 3, y + 6, 1);
+  rect(x + 3, y + 6, 1);
+  rect(x + 1, y + 18, 1);
+  rect(x - 2, y + 17, 1, π / 2);
+}
+
+man(hw, hh);
+
+function ex(x, y, w, h) {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'white';
+  ctx.moveTo(x, y);
+  ctx.lineTo(w, h);
+  ctx.moveTo(w, y);
+  ctx.lineTo(x, h);
+  ctx.stroke();
+}
+
+// ex(0, 0, w, h);
